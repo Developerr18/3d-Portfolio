93adaf252b39e6e5cf6a9bc77436534fd53abdaa
Developerr18 <dineshnikum1@gmail.com>
2025-10-01 19:09:42 +0530

    initial files code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <main className="bg-slate-300/20">
+      <Navbar />
+      <h1 className="text-red-500">Hello World</h1>
+    </main>
+  );
+}
+
+export default App;
